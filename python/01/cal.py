6b6081a46174555ccf05624e89caa62f6db8a35c
Marcos Hernandez <marcos@mhndz.com>
2023-06-04 10:45:06 -0400

    changes but not solved

--- python/01/cal.py
+++ python/01/cal.py
@@ -2,23 +2,22 @@ text_file = open("input.txt", "r")
 contents = text_file.read()
 cal_list =contents.split('\n') 
 cur_total = 0
-high_score = 0
+high_scores = [0,0,0]
+
 for cal in cal_list:
     if cal != '':
         cur_total = cur_total + int(cal)
-    if cal == '':
-        if cur_total > high_score:
-            high_score = cur_total
-            print("New High Score ",high_score)
-            cur_total = 0
-        elif cur_total < high_score:
-            print("THE HIGH SCORE STANDS ", high_score)
-            cur_total = 0
-        elif cur_total == high_score:
-            print("HIGH SCORE HAS BEEN MATCHED")
-            cur_total=0
+    elif cal == '':
+        for i in range(len(high_scores)):
+            if cur_total > high_scores[i]:
+                high_scores[i] = cur_total
+                break
+            
 
-print("THIS IS THE HIGH SCORE: ",high_score)
+        cur_total = 0
+
+print("Hight Scores: ",high_scores)
+print("Top 3 Total: ", sum(high_scores))
 
         
         
